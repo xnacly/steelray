671f204d983239b4ece8e51ec269246865f8cc89
xnacly <47723417+xNaCly@users.noreply.github.com>
2026-05-01 21:10:05 +0200

    nothing works, i dont even know which parts to hate anymore

--- Makefile
+++ Makefile
@@ -1,24 +1,21 @@
 TARGET = aarch64-unknown-none
 
 all:
-	aarch64-linux-gnu-as -o start.o start.S
 	rustc \
 		--target $(TARGET) \
 		-C panic=abort \
 		-C overflow-checks=off \
-		-C link-arg=start.o \
-		-C linker=aarch64-linux-gnu-ld \
-		-C link-arg=-Tlinker.ld \
+	    -C link-arg=-Tlinkmyballs.ld \
 		-o kernel.elf \
 		src/main.rs
 
 run: all
 	qemu-system-aarch64 \
-		-M virt \
-		-cpu cortex-a57 \
-		-nographic \
-		-S -s \
-		-kernel kernel.elf
+	  -M virt \
+	  -cpu cortex-a57 \
+	  -nographic \
+	  -S -s \
+	  -kernel kernel.elf
 
 clean:
-	rm kernel.elf start.o
+	rm kernel.elf
--- src/main.rs
+++ src/main.rs
@@ -3,13 +3,38 @@
 
 use core::panic::PanicInfo;
 
+const UARTDR: *mut u32 = 0x09000000 as *mut u32;
+const UARTCR: *mut u32 = 0x09000030 as *mut u32;
+const UARTLCRH: *mut u32 = 0x0900002C as *mut u32;
+
+fn uart_init() {
+    unsafe {
+        UARTCR.write_volatile(0);
+        UARTLCRH.write_volatile(1 << 4);
+        UARTCR.write_volatile((1 << 0) | (1 << 8) | (1 << 9));
+    }
+}
+
+fn uart_putc(c: u8) {
+    unsafe {
+        UARTDR.write_volatile(c as u32);
+    }
+}
+
+fn kprint(s: &str) {
+    for b in s.bytes() {
+        uart_putc(b)
+    }
+}
+
 #[unsafe(no_mangle)]
-pub extern "C" fn kmain() -> ! {
-    let p = 0x40100000 as *mut u64;
+pub extern "C" fn _start() -> ! {
+    // uart_init();
+    // kprint("i want to kill myself");
+    let p = 0x40000000 as *mut u64;
     unsafe {
         *p = 0xDEADAFFE;
     }
-
     loop {}
 }
 
